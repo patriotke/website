e48e504677988d556dc53c6e0027c2f19d9e7737
Pat <pat@patriotke.com>
2024-06-23 20:34:39 +0300

    add tests

--- src/app/tests/landingpage.test.tsx
+++ src/app/tests/landingpage.test.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import LandingPage from '../page';
 
 describe('LandingPage', () => {
-    it('renders LandingPage', () => {
-        render(<LandingPage />)
+  it('renders LandingPage', () => {
+    render(<LandingPage />);
 
-        const { container } = render(<LandingPage />)
-        expect(container).toMatchSnapshot()
-    })
-})
+    const { container } = render(<LandingPage />);
+    expect(container).toMatchSnapshot();
+  });
+});
